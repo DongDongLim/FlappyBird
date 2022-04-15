63b1ea37802b30df581ccf531bd9eeb7464b2adc
DongDongLim <99638155+DongDongLim@users.noreply.github.com>
2022-04-15 17:41:12 +0900

    1
    1

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField]
+    float m_jumpImpact;
+
+    Rigidbody2D m_rigidbody2D;
+    Animator m_animator;
+
+    private void Start()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_rigidbody2D.velocity = Vector2.zero;
+            m_rigidbody2D.AddForce(Vector2.up * m_jumpImpact, ForceMode2D.Impulse);
+
+        }
+        if (m_rigidbody2D.velocity.y < -1)
+            m_rigidbody2D.velocity = Vector2.down;
+        if(m_rigidbody2D.velocity.y > 0)
+        {
+            if (!m_animator.GetBool("IsUp"))
+                m_animator.SetBool("IsUp", true);
+        }
+        else
+        {
+            if (m_animator.GetBool("IsUp"))
+                m_animator.SetBool("IsUp", false);
+        }
+
+    }
+
+}
---
+++ Assets/Script/GameMng.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMng : MonoBehaviour
+{
+    int m_gameSpeed = 1;
+
+    public GameObject m_hurdleObj;
+
+    Queue<GameObject> m_hurdleObjPooling = new Queue<GameObject>();
+
+    private void Start()
+    {
+        for (int i = 0; i < 3; ++i)
+            m_hurdleObjPooling.Enqueue(Instantiate(m_hurdleObj, GetComponent<Transform>(), false));
+    }
+
+}
